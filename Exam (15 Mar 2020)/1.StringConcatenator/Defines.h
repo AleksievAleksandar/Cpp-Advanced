f814c6327154038aa88576e82af9595ae524f69f
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2020-03-28 12:59:31 +0200

    Create Defines.h

---
+++ Exam (15 Mar 2020)/1.StringConcatenator/Defines.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef DEFINES_H_
+#define DEFINES_H_
+
+enum class ConcatenateStrategy
+{
+	LEFT_1_RIGHT_1 = 0,
+	LEFT_2_RIGHT_1 = 1,
+	LEFT_1_RIGHT_2 = 2
+};
+
+
+
+#endif /* DEFINES_H_ */
