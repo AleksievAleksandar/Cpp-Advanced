e12a28f965293582f0bd3c30b070dd176e095210
Aleksandar Aleksiev <59371916+AleksievAleksandar@users.noreply.github.com>
2021-02-15 18:50:17 +0200

    Create MemoryPool.cpp

---
+++ Exam (29 February 2020)/3.MemoryPool/Updated_Solution/MemoryPool.cpp
@@ -0,0 +1,98 @@
+#include "MemoryPool.h"
+
+#include <cstdlib>
+
+MemoryPool::MemoryPool(const int memoryPoolSize):
+	_memoryBuffer(new int[memoryPoolSize] {}), 
+	_isMemoryBufferOccupied(new bool[memoryPoolSize] {}),
+	_MEMORY_POOL_SIZE(memoryPoolSize)
+{
+}
+
+MemoryPool::~MemoryPool()
+{
+	delete[] this->_memoryBuffer;
+	delete[] this->_isMemoryBufferOccupied;
+}
+
+ErrorCode MemoryPool::requestMemory(MemoryNode& outNode)
+{
+	if (outNode.size > this->_MEMORY_POOL_SIZE)
+	{
+		return ErrorCode::REQUEST_FAILURE_BIGGER_THAN_BUFFER;
+	}
+
+	size_t haveEnoughFreeSpace = 0;
+	size_t startIndexForNode = 0;
+	bool findStartIndex = false;
+
+	for (size_t i = 0; i < this->_MEMORY_POOL_SIZE; i++)
+	{
+		if (!this->_isMemoryBufferOccupied[i])
+		{
+			haveEnoughFreeSpace++;
+			if (!findStartIndex)
+			{
+				startIndexForNode = i;
+				findStartIndex = true;
+			}
+
+			if (haveEnoughFreeSpace == outNode.size)
+			{
+				break;
+			}
+		}
+		else if (this->_isMemoryBufferOccupied[i] && haveEnoughFreeSpace < outNode.size)
+		{
+			haveEnoughFreeSpace = 0;
+			findStartIndex = false;
+		}
+	}
+	if (haveEnoughFreeSpace < outNode.size)
+	{
+		return ErrorCode::REQUEST_FAILURE_NOT_ENOUGH_MEMORY;
+	}
+
+	outNode.memory = this->_memoryBuffer + startIndexForNode;
+	outNode.bufferStartIndex = startIndexForNode;
+
+	this->occupyMemory(outNode);
+	
+	return ErrorCode::REQUEST_SUCCESS;
+}
+
+void MemoryPool::releaseMemory(const MemoryNode& outNode)
+{
+	size_t cnt = outNode.bufferStartIndex;
+	size_t howManySteps = outNode.size;
+	while (howManySteps > 0)
+	{
+		this->_isMemoryBufferOccupied[cnt] = false;
+		cnt++;
+		howManySteps--;
+	}
+}
+
+void MemoryPool::zeroMemoryValue(const MemoryNode& node)
+{
+	size_t cnt = node.bufferStartIndex;
+	size_t howManySteps = node.size;
+	while (howManySteps > 0)
+	{
+		this->_memoryBuffer[cnt] = 0;
+		cnt++;
+		howManySteps--;
+	}
+}
+
+void MemoryPool::occupyMemory(const MemoryNode& node)
+{
+	size_t cnt = node.bufferStartIndex;
+	size_t howManySteps = node.size;
+	while (howManySteps > 0)
+	{
+		this->_isMemoryBufferOccupied[cnt] = true;
+		cnt++;
+		howManySteps--;
+	}
+}
